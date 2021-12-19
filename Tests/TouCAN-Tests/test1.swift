294dca625ea9af4575858c78c5416b07f65e0e79
Dr. Michael Lauer <mickey@vanille-media.de>
2021-12-19 13:16:30 +0100

    Swift: quit test after receiving 10 frames

--- Tests/TouCAN-Tests/test1.swift
+++ Tests/TouCAN-Tests/test1.swift
@@ -9,10 +9,15 @@ class ReadTest: XCTestCase {
             try toucan.connect(bitrate: 500000)
             let getVin = TouCAN.Message(id: 0x7DF, data: [0x02, 0x09, 0x02])
             try toucan.writeMessage(getVin)
+            var received: Int = 0
             while true {
                 print("waiting to read the next message...")
                 let message = try toucan.readMessage()
                 print("received message: \(message)")
+                received += 1
+                if received == 10 {
+                    break
+                }
             }
         } catch {
             print("Error: \(error)")
